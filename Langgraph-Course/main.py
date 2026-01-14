cc0ea9b2d24bbf34d2eb034c53bf31c0a4575e22
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-14 21:33:20 +0800

    Learned TypedDict

--- Langgraph-Course/main.py
+++ Langgraph-Course/main.py
@@ -1,1 +1,20 @@
-print('Learn Langgraph')
+from typing import TypedDict, List
+
+class Crush(TypedDict):
+    name : str
+    year : int
+
+crush_list : List[Crush] = []
+
+
+for i in range(3):
+    name = input('Enter Crush Name : ')
+    year = int(input('Enter Year : '))
+    print()
+
+    new_crush = Crush(name=name, year=year)
+
+    crush_list.append(new_crush)
+
+
+print(crush_list)

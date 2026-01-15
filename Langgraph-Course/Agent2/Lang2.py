609aa7a2a24f1595d837510b067c9fd2d321a59c
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-15 22:39:57 +0800

    Added process_ans function for handling logic

--- Langgraph-Course/Agent2/Lang2.py
+++ Langgraph-Course/Agent2/Lang2.py
@@ -1,3 +1,21 @@
 from langgraph.graph import StateGraph
 from typing import List, TypedDict
 import numpy as np
+
+class AgentState(TypedDict):
+  nums : List[int]
+  name : str
+  operation : str
+  result : str
+
+  def process_ans(state: AgentState) -> AgentState:
+    """Handles the Logic & handles multiple inputs"""
+
+    if state['operation'] == '+':
+        state['result'] = f"\nName : {state['name']} \nOperator : {state['operation']} \nResults : {np.sum(state['nums'])}"
+    elif state['operation'] == '*':
+        state['result'] = f"\nName : {state['name']} \nOperator : {state['operation']} \nResults : {np.prod(state['nums'])}"
+    else:
+        state['result'] = "\nInvalid Operator"
+
+    return state

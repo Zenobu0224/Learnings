071700c359c512e655e34bb8949973e7448b9eef
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-15 17:05:15 +0800

    Added TypedDict for better dictionary

--- Langgraph-Course/main.py
+++ Langgraph-Course/main.py
@@ -2,29 +2,4 @@ from typing import Dict, TypedDict
 from langgraph.graph import StateGraph
 
 
-class AgentState(TypedDict):
-  msg : str
 
-
-def greeting_node(state: AgentState) -> AgentState:
-  """Simple node that adds a greeting message to the state """
-
-  state['msg'] = 'Hello ' + state['msg'] + ', hows your day?'
-
-  return state
-
-
-graph = StateGraph(AgentState)
-
-graph.add_node('greeter', greeting_node)
-
-graph.set_entry_point('greeter')
-graph.set_finish_point('greeter')
-
-app = graph.compile()
-
-name = input('Enter Name : ')
-
-result = app.invoke({'msg' : name})
-
-print(result['msg'])

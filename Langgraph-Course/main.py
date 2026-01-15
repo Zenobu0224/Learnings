a466233bd41025d947cd3d7490ea7a1bfcc72700
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-15 17:02:01 +0800

    Added some essential libraries for exploring langgraph

--- Langgraph-Course/main.py
+++ Langgraph-Course/main.py
@@ -1,9 +1,30 @@
-from typing import Optional
+from typing import Dict, TypedDict
+from langgraph.graph import StateGraph
 
-def message(msg : Optional[str]) -> None:
-    if msg is None:
-        print('Eden Pearl')
-    else:
-        print(f'{msg} <3')
 
-message(msg="Devine Lorraine mahal parin kita kahit maldita ka!!")
+class AgentState(TypedDict):
+  msg : str
+
+
+def greeting_node(state: AgentState) -> AgentState:
+  """Simple node that adds a greeting message to the state """
+
+  state['msg'] = 'Hello ' + state['msg'] + ', hows your day?'
+
+  return state
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node('greeter', greeting_node)
+
+graph.set_entry_point('greeter')
+graph.set_finish_point('greeter')
+
+app = graph.compile()
+
+name = input('Enter Name : ')
+
+result = app.invoke({'msg' : name})
+
+print(result['msg'])

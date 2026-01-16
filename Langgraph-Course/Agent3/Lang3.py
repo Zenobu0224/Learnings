0f210b07b8dea1f3728a56a89b854db66f4f53f2
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-16 13:04:58 +0800

    Added function that serves as nodes to the StateGraph

--- Langgraph-Course/Agent3/Lang3.py
+++ Langgraph-Course/Agent3/Lang3.py
@@ -3,6 +3,30 @@ from typing import List, TypedDict
 
 class AgentState(TypedDict):
   name : str
-  year : str
-  course : str
-  final : str
+  age : str
+  skills : List[str]
+  output : str
+  
+
+def node_one(state : AgentState) -> AgentState:
+    """First node that process the name"""
+
+    state['output'] = f"Hello {state['name']}, Welcome to the system! "
+
+    return state
+
+
+def second_node(state: AgentState) -> AgentState:
+    """Second node that process the age"""
+
+    state['output'] = state['output'] + f'\nYou are {state['age']} years old! '
+
+    return state
+
+
+def third_node(state: AgentState) -> AgentState:
+    """Third node that process the skills"""
+
+    state['output'] = state['output'] + f'\nYou have skiils in: {', '.join(state['skills'])}'
+
+    return state

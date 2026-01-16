b643be472c17497e594ffabd87d437d2736f16a3
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-16 21:17:04 +0800

    Added AgentState with dependencies

--- Langgraph-Course/Agent4/ConditionalEdge.py
+++ Langgraph-Course/Agent4/ConditionalEdge.py
@@ -1,3 +1,15 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict
 import numpy as np
+
+class AgentState(TypedDict):
+    num1 : int
+    operation1 : str
+    num2 : int
+
+    num3 : int
+    operation2 : str
+    num4 : int
+
+    result1 : str
+    result2 : str

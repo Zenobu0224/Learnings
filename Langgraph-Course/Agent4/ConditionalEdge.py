bbb2029b709a2da877a1def2f73c325755af82db
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-16 21:17:54 +0800

    Added functions that acts as a node

--- Langgraph-Course/Agent4/ConditionalEdge.py
+++ Langgraph-Course/Agent4/ConditionalEdge.py
@@ -13,3 +13,38 @@ class AgentState(TypedDict):
 
     result1 : str
     result2 : str
+
+
+def add_node1(state: AgentState) -> AgentState:
+    state["result1"] = f"{state['num1']} + {state['num2']} = {state['num1'] + state['num2']}"
+
+    return state
+
+def sub_node1(state: AgentState) -> AgentState:
+    state["result1"] = f"{state['num1']} - {state['num2']} = {state['num1'] - state['num2']}"
+
+    return state
+
+def conditional_edge1(state: AgentState) -> AgentState:
+    if state["operation1"] == "+":
+        return "add_edge1"
+    elif state["operation1"] == "-":
+        return "sub_edge1"
+
+def add_node2(state: AgentState) -> AgentState:
+    state["result2"] = f"{state['num3']} + {state['num4']} = {state['num3'] + state['num4']}"
+    
+    return state
+
+
+def sub_node2(state: AgentState) -> AgentState:
+    state["result2"] = f"{state['num3']} - {state['num4']} = {state['num3'] - state['num4']}"
+
+    return state
+
+
+def conditional_edge2(state: AgentState) -> AgentState:
+    if state["operation2"] == "+":
+        return "add_edge2"
+    elif state["operation2"] == "-":
+        return "sub_edge2"

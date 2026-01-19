ae08f552725d4b646a66e8e40330cec7b4b9d523
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-19 23:07:50 +0800

    Added AgentState

--- Langgraph-Course/Agent5/LoopingGraph.py
+++ Langgraph-Course/Agent5/LoopingGraph.py
@@ -2,3 +2,7 @@ from langgraph.graph import StateGraph, END
 from typing import TypedDict, List
 import random
 
+class AgentState(TypedDict):
+    name : str
+    nums : List[int]
+    counter : int

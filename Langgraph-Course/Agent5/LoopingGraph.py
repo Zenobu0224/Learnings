8b3ce6f34e1be57fc571a7f2d182ec7637aaecaf
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-19 23:08:28 +0800

    Added essential functions for the lesson

--- Langgraph-Course/Agent5/LoopingGraph.py
+++ Langgraph-Course/Agent5/LoopingGraph.py
@@ -6,3 +6,21 @@ class AgentState(TypedDict):
     name : str
     nums : List[int]
     counter : int
+
+def greeter_node(state: AgentState) -> AgentState:
+    state['name'] = f"Hello {state['name']}, how's your day?"
+    state['counter'] = 0
+
+    return state
+
+def random_node(state: AgentState) -> AgentState:
+    state['nums'].append(random.randint(0, 24))
+    state['counter'] += 1
+    return state
+
+
+def conditional_node(state: AgentState) -> AgentState:
+    if state['counter'] < 7:
+        return "loop"
+    else:
+        return "exit"

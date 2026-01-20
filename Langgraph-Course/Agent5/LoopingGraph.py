1fde8806818acd9756ae1ec010156b5a64b83b5b
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-20 20:00:24 +0800

    Initialize GameState class

--- Langgraph-Course/Agent5/LoopingGraph.py
+++ Langgraph-Course/Agent5/LoopingGraph.py
@@ -1,3 +1,12 @@
 from langgraph.graph import StateGraph, END
 from typing import TypedDict, List
 from numpy import randoms
+
+
+class GameState(TypedDict):
+  player_name : str
+  target_num : int
+  guesses : List[int]
+  attempts : int
+  lower_bound : int
+  upper_bound : int

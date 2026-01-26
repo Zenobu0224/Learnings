66f3e6e7f64b06b912559a69029b76761a08b02e
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-26 15:31:55 +0800

    Added AgentState

--- Langgraph-Course/AI-Agent-1/aiagent1.py
+++ Langgraph-Course/AI-Agent-1/aiagent1.py
@@ -1,4 +1,8 @@
 from typing import TypedDict, List
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_ollama import ChatOllama
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START,  END
+
+
+class AgentState(TypedDict):
+    messages : List[BaseMessage]

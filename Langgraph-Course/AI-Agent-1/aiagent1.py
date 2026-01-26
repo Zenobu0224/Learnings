4e782c6c184537e1fde26ae16a066bd58ce8e374
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-26 15:33:51 +0800

    Added LLM Model(Llama3.2:1b)

--- Langgraph-Course/AI-Agent-1/aiagent1.py
+++ Langgraph-Course/AI-Agent-1/aiagent1.py
@@ -3,6 +3,7 @@ from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START,  END
 
-
 class AgentState(TypedDict):
     messages : List[BaseMessage]
+
+llm = ChatOllama(model="llama3.2:1b")

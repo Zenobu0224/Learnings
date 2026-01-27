a436b8f3817be6c5ed2506e4d27dae6ac6f9b3b2
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-27 11:24:04 +0800

    Added process node

--- Langgraph-Course/AI-Agent-2/aiagent2.py
+++ Langgraph-Course/AI-Agent-2/aiagent2.py
@@ -9,3 +9,11 @@ class AgentState(TypedDict):
 
 
 llm = ChatOllama(model="llama3.2:1b")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state['messages'])
+
+    state['messages'].append(AIMessage(content=response.content))
+    print(f"AI : {response.content}\n")
+
+    return state

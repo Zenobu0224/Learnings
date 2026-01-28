37680c4d0c379625cf9693d2b9ca25ef50765912
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-28 21:51:33 +0800

    Added AgentState

--- Langgraph-Course/AI-Agent-3/aiagent3.py
+++ Langgraph-Course/AI-Agent-3/aiagent3.py
@@ -5,3 +5,7 @@ from langchain_core.tools import tool
 from langgraph.graph.message import add_message
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
+
+
+class AgentState(TypedDict):
+    messages : Annotated[Sequence[BaseMessage], add_message]

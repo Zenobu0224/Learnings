091db2e5f3b67c846a43ccfd22e2e3d4c22aad2d
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-02-09 15:52:40 +0800

    Created save tool

--- Langgraph-Course/AI-Agent-4/aiagent4.py
+++ Langgraph-Course/AI-Agent-4/aiagent4.py
@@ -11,6 +11,7 @@ document_content = ""
 
 class AgentState(TypedDict):
     messages : Annotated[Sequence[BaseMessage], add_messages]
+    
 
 @tool
 def update(content: str) -> str:
@@ -19,3 +20,24 @@ def update(content: str) -> str:
     document_content = content
 
     return f"Document has been updated successfully! The current content is : \n{document_content}"
+
+@tool
+def save(filename: str) -> str:
+    """Save the current document to a text file and finish the process.
+    
+    Args : 
+        filename : Name for the text file."""
+    
+    global document_content
+    
+    if not filename.endswith('.txt'):
+        filename = f"{filename}.txt"
+
+    try:
+        with open(filename, "w") as file:
+            file.write(document_content)
+        print(f"\nDocument has been saved to : {filename}")
+
+        return f"Document has been saved successfully to {filename}."
+    except Exception as e:
+        return f"Error saving document {str(e)}"

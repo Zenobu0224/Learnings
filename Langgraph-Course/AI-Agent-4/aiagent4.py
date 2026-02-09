6b977eebde9fe770a7d8e09d6fea9dbaa8f10f60
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-02-09 15:35:28 +0800

    Added essential tools for drafter-ai-agent

--- Langgraph-Course/AI-Agent-4/aiagent4.py
+++ Langgraph-Course/AI-Agent-4/aiagent4.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage, ToolMessage
+from langchain_ollama import ChatOllama
+from typing import Annotated, Sequence, TypedDict
+from langchain_core.tools import tool
+from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
+
